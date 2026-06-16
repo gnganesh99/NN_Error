bfa58ad41805b3bde5d20843f8c03f9174c321ff
ajoshi20-1 <ajoshi20@vols.utk.edu>
2026-06-16 11:04:38 -0400

    modified plot_spectra legend

--- src/nnerror/plot_functions.py
+++ src/nnerror/plot_functions.py
@@ -523,7 +523,7 @@ def plot_spectra(pred_spectra, orig_spectrum, error_val, expt_name = 'test_expt'
 
 
     for spectrum in pred_spectra:
-        ax.plot(x, spectrum, linewidth = 3, label = f'Predicted\nL1-error: {error_val:.4f}')
+        ax.plot(x, spectrum, linewidth = 3, label = f'Predicted\n{error_type}-error: {error_val:.4f}')
     ax.legend(fontsize=10)
 
     if to_save:
