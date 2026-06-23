e2407d98ba3fad21f6399eb76acb9af4cb98615f
ajoshi20-1 <ajoshi20@vols.utk.edu>
2026-06-23 10:25:04 -0400

    Updated train_model function
    Changed default value of partial_train from True to False in train_model function.

--- src/nnerror/training_functions.py
+++ src/nnerror/training_functions.py
@@ -26,8 +26,8 @@ def norm_0to1(arr):
 
 # Model training function with swa updates
   
-def train_model(model, dataset, n_batches= 3, lr = 0.1, patience = 10, n_epochs = 100, partial_train = True,
-                batchsize = None, val_dataset =  None, use_swa = True, last_swa_epochs = 0.1):
+def train_model(model, dataset, n_batches= 3, lr = 0.1, patience = 10, n_epochs = 100, partial_train = False,
+                batchsize = None, use_swa = True, last_swa_epochs = 0.1):
  
     """
     Train a model with optional encoder freezing, early stopping, and a
